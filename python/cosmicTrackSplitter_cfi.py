d20cb35baf180d300b37172f1680fab5428dea48
cmsbuild <>
2009-01-26 09:43:13 +0000

    This commit was manufactured by cvs2git to create tag 'RecoTracker- FinalTrackSelectors-CMSSW_3_1_0_pre1'.
    Sprout from master 2008-10-12 19:58:15 UTC Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch> 'spitting of 1st tracking step into 2 sub-steps'
    Cherrypick from master 2009-01-26 09:43:08 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'add top/bottom hit filter from Giuseppe.':
        python/TrackCandidateTopBottomHitFilter_cfi.py
        python/cosmicTrackSplitter_cfi.py
        src/CosmicTrackSplitter.cc
        src/TrackCandidateTopBottomHitFilter.cc
    Delete:
        data/MergeTrackCollections.cff
        data/TracksWithQuality.cff
        data/ctfMCTrackListCleaner.cff
        data/ctfMCTrackListCleaner.cfi
        data/ctfTIFTrackListCleaner.cff
        data/ctfTIFTrackListCleaner.cfi
        data/ctfrsMCTrackListMerger.cff
        data/ctfrsMCTrackListMerger.cfi
        data/ctfrsTIFTrackListMerger.cff
        data/ctfrsTIFTrackListMerger.cfi
        data/ctfrsTrackListMerger.cff
        data/ctfrsTrackListMerger.cfi
        data/cutsCKF.cfi
        data/rsMCTrackListCleaner.cff
        data/rsMCTrackListCleaner.cfi
        data/rsTIFTrackListCleaner.cff
        data/rsTIFTrackListCleaner.cfi
        data/selectCKF.cfi
        data/selectHighPurity.cfi
        data/selectLoose.cfi
        data/selectTight.cfi
        data/selectTighter.cfi
        test/RelValTTbar.cff
        test/merger.cfg
        test/tau.cfg
        test/testTrackMultiSelector.cfg

---
+++ python/cosmicTrackSplitter_cfi.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+cosmicTrackSplitter = cms.EDFilter("CosmicTrackSplitter",
+                                   stripFrontInvalidHits = cms.bool(True),
+                                   stripBackInvalidHits = cms.bool(True),
+                                   stripAllInvalidHits = cms.bool(False),
+                                   replaceWithInactiveHits = cms.bool(False),
+                                   tracks = cms.InputTag("cosmictrackfinderP5"),
+                                   tjTkAssociationMapTag = cms.InputTag("cosmictrackfinderP5"),
+                                   minimumHits = cms.uint32(6),
+                                   detsToIgnore = cms.vuint32(),
+                                   dzCut = cms.double( 9999.0 ),
+                                   dxyCut = cms.double( 9999.0 )
+                                   )
+
