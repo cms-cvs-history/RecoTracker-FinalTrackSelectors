dcc8df468e4dddf1c23b490ea0e717619873d0fb
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2012-04-15 14:19:47 +0000

    Some 52x backports for 428_SLHC releases

--- src/TrackListMerger.cc
+++ src/TrackListMerger.cc
@@ -8,8 +8,8 @@
 // Created:         April 4, 2011
 //
 // $Author: dlange $
-// $Date: 2011/05/28 02:33:06 $
-// $Revision: 1.5 $
+// $Date: 2011/06/01 03:02:04 $
+// $Revision: 1.6 $
 //
 
 #include <memory>
@@ -80,6 +80,8 @@ namespace cms
     epsilon_ =  conf.getParameter<double>("Epsilon");
     shareFrac_ =  conf.getParameter<double>("ShareFrac");
     allowFirstHitShare_ = conf.getParameter<bool>("allowFirstHitShare");
+    foundHitBonus_ = conf.getParameter<double>("FoundHitBonus");
+    lostHitPenalty_ = conf.getParameter<double>("LostHitPenalty");
     std::string qualityStr = conf.getParameter<std::string>("newQuality");
  
     if (qualityStr != "") {
@@ -241,12 +243,16 @@ namespace cms
     //cache the rechits and valid hits
     std::vector<std::vector<const TrackingRecHit*> > rh1(rSize);
     std::vector<int> validHits(rSize,0);
+    std::vector<int> lostHits(rSize,0);
     for ( unsigned int i=0; i<rSize; i++) {
+      //validHits[i]=0;
+      //lostHits[i]=0;
       if (selected[i]==0) continue;
       unsigned int collNum=trackCollNum[i];
       unsigned int trackNum=i-trackCollFirsts[collNum];
       const reco::Track *track=&((trackColls[collNum])->at(trackNum)); 
       validHits[i]=track->numberOfValidHits();
+      lostHits[i]=track->numberOfLostHits();
 
       rh1[i].reserve(track->recHitsSize());
       for (trackingRecHit_iterator it = track->recHitsBegin();  it != track->recHitsEnd(); ++it) { 
@@ -277,6 +283,7 @@ namespace cms
 	int qualityMaskT1 = trackQuals[i];
 
 	int nhit1 = validHits[i];
+        int lhit1 = lostHits[i];
 
 	for ( unsigned int j=i+1; j<rSize; j++) {
 	  if (selected[j]==0) continue;
@@ -324,40 +331,31 @@ namespace cms
 	    newQualityMask =(maskT1 | maskT2); // take OR of trackQuality 
 	  }
 	  int nhit2 = validHits[j];
+          int lhit2 = lostHits[j];
 
 	  if ( (noverlap-firstoverlap) > (std::min(nhit1,nhit2)-firstoverlap)*shareFrac_ ) {
-	    if ( nhit1 > nhit2 ){
+            double score1 = foundHitBonus_*nhit1 - lostHitPenalty_*lhit1 - track->chi2();
+            double score2 = foundHitBonus_*nhit2 - lostHitPenalty_*lhit2 - track2->chi2();
+            const double almostSame = 1.001;
+            if ( score1 > almostSame * score2 ) {
 	      selected[j]=0; 
 	      selected[i]=10+newQualityMask; // add 10 to avoid the case where mask = 1
 	      trkUpdated[i]=true;
+            } else if ( score2 > almostSame * score1 ) {
+	      selected[i]=0; 
+	      selected[j]=10+newQualityMask;  // add 10 to avoid the case where mask = 1
+	      trkUpdated[j]=true;
 	    }else{
-	      if ( nhit1 < nhit2 ){
-		selected[i]=0; 
-		selected[j]=10+newQualityMask;  // add 10 to avoid the case where mask = 1
-		trkUpdated[j]=true;
+	      // If tracks from both iterations are virtually identical, choose the one from the first iteration.
+	      if (track->algo() <= track2->algo()) {
+	        selected[j]=0;
+	        selected[i]=10+newQualityMask; // add 10 to avoid the case where mask = 1
+	        trkUpdated[i]=true;
 	      }else{
-		const double almostSame = 1.001;
-		if (track->normalizedChi2() > almostSame * track2->normalizedChi2()) {
-		  selected[i]=0;
-		  selected[j]=10+newQualityMask; // add 10 to avoid the case where mask = 1
-		  trkUpdated[j]=true;
-		}else if (track2->normalizedChi2() > almostSame * track->normalizedChi2()) {
-		  selected[j]=0;
-		  selected[i]=10+newQualityMask; // add 10 to avoid the case where mask = 1
-		  trkUpdated[i]=true;
-		}else{
-		  // If tracks from both iterations are virtually identical, choose the one from the first iteration.
-		  if (track->algo() <= track2->algo()) {
-		    selected[j]=0;
-		    selected[i]=10+newQualityMask; // add 10 to avoid the case where mask = 1
-		    trkUpdated[i]=true;
-		  }else{
-		    selected[i]=0;
-		    selected[j]=10+newQualityMask; // add 10 to avoid the case where mask = 1
-		    trkUpdated[j]=true;
-		  }
-		}
-	      }//end fi > or = fj
+	        selected[i]=0;
+	        selected[j]=10+newQualityMask; // add 10 to avoid the case where mask = 1
+	        trkUpdated[j]=true;
+	      }
 	    }//end fi < fj
 	  }//end got a duplicate
 	  //stop if the ith track is now unselected
